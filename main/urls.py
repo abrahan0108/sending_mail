e8eade92ef7b647c0057c5e0858b389128ad9e07
Abrahan <f8abrahan@gmail.com>
2019-04-29 11:20:10 -0500

    Subo a github

--- main/urls.py
+++ main/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path
 from django.views.generic import TemplateView
-from main import views
+from .views import sending_mail
 
 
 urlpatterns = [
     path("",
          TemplateView.as_view(template_name="home.html"),
          name="home"),
-    path("contact/",
-         views.ContactUsView.as_view(),
-         name="contact"),
+    path("send_email/",
+         sending_mail),
 ]
--- main/views.py
+++ main/views.py
@@ -1,12 +1,15 @@
 from django.views.generic.edit import FormView
 from main import forms
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.core.mail import send_mail
 # Create your views here.
 
-class ContactUsView(FormView):
-    template_name = "main/contact.html"
-    form_class = forms.ContactForm
-    success_url = "/"
 
-    def form_valid(self, form):
-        form.send_mail()
-        return super().form_valid()
+def sending_mail(request):
+    if request.method == 'POST':
+        email = request.POST.get("email")
+        send_mail('Subject', 'message', 'f8abrahan@gmail.com', [email], fail_silently=False)
+        return HttpResponse('Mail Sent to' + email)
+
+    return render(request, 'main/send_email.html')
